96e0300ef631fbf75680eed435b55bd08f52bdfb
Jhow Gama <jonathan.nascimento.external@lojasrenner.com.br>
2023-12-19 18:49:31 -0400

    add fontFamily the string

--- src/designer-system/components/typography/text/index.tsx
+++ src/designer-system/components/typography/text/index.tsx
@@ -1,15 +1,18 @@
-import React from "react";
-import styled from "styled-components/native";
-import { TextStyle, TextProps } from "react-native";
+import React from 'react';
+import styled from 'styled-components/native';
+import { TextStyle, TextProps } from 'react-native';
 
-interface DsTextProps extends TextStyle, TextProps {
-    fontFamily?:
-        | "Inter_300Light"
-        | "Inter_400Regular"
-        | "Inter_500Medium"
-        | "Inter_700Bold"
-        | "Inter_900Black";
-}
+type FontFamily =
+    | 'Inter_300Light'
+    | 'Inter_400Regular'
+    | 'Inter_500Medium'
+    | 'Inter_700Bold'
+    | 'Inter_900Black';
+
+type DsTextProps = TextStyle &
+    TextProps & {
+        fontFamily?: FontFamily | string;
+    };
 
 const StyledText = styled.Text<DsTextProps>``;
 
