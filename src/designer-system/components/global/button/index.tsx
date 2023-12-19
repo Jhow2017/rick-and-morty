b8b5d1041d25305303c42731d702fa268658a8fc
Jhow Gama <jonathan.nascimento.external@lojasrenner.com.br>
2023-12-19 18:49:54 -0400

    add new color textStyles

--- src/designer-system/components/global/button/index.tsx
+++ src/designer-system/components/global/button/index.tsx
@@ -1,21 +1,21 @@
-import React from "react";
+import React from 'react';
 import {
     TouchableOpacity,
     TouchableOpacityProps,
     ViewStyle,
     ActivityIndicator,
     TextStyle,
-} from "react-native";
+} from 'react-native';
 
 //components
-import { DsText } from "@ds/components/typography";
-import DsIcon, { IconsType } from "@ds/components/global/icon";
-import { filterProps } from "src/services/utils/filterProps";
+import { DsText } from '@ds/components/typography';
+import DsIcon, { IconsType } from '@ds/components/global/icon';
+import { filterProps } from 'src/services/utils/filterProps';
 
 interface DsButtonProps extends TouchableOpacityProps, TextStyle, ViewStyle {
     children: React.ReactNode;
-    variant: "default" | "primary" | "secondary";
-    size: "small" | "medium" | "large";
+    variant: 'default' | 'primary' | 'secondary';
+    size: 'small' | 'medium' | 'large';
     loading?: boolean;
     icon?: IconsType;
     iconLeft?: boolean;
@@ -27,29 +27,29 @@ const DsButton: React.FC<DsButtonProps> = (props) => {
     const buttonStyles: ViewStyle = {
         borderRadius: 30,
         padding:
-            (size === "small" && 4) ||
-            (size === "medium" && 12) ||
-            (size === "large" && 30) ||
+            (size === 'small' && 4) ||
+            (size === 'medium' && 12) ||
+            (size === 'large' && 30) ||
             0,
-        alignItems: "center",
-        justifyContent: "center",
+        alignItems: 'center',
+        justifyContent: 'center',
         width:
-            (size === "small" && 120) ||
-            (size === "medium" && 220) ||
-            (size === "large" && 329) ||
-            "auto",
+            (size === 'small' && 120) ||
+            (size === 'medium' && 220) ||
+            (size === 'large' && 329) ||
+            'auto',
         height:
-            (size === "small" && 40) ||
-            (size === "medium" && 52) ||
-            (size === "large" && 92) ||
-            "auto",
+            (size === 'small' && 40) ||
+            (size === 'medium' && 52) ||
+            (size === 'large' && 92) ||
+            'auto',
         backgroundColor:
-            variant === "default"
-                ? "transparent"
-                : variant === "primary"
-                ? "blue"
-                : variant === "secondary"
-                ? "#42C83C"
+            variant === 'default'
+                ? 'transparent'
+                : variant === 'primary'
+                ? 'blue'
+                : variant === 'secondary'
+                ? '#87F54E'
                 : attr?.backgroundColor,
         ...(attr as ViewStyle),
     };
@@ -64,11 +64,11 @@ const DsButton: React.FC<DsButtonProps> = (props) => {
         fontSize: attr.fontSize ?? textSize[size],
         color:
             attr.color ??
-            (variant === "primary"
-                ? "red"
-                : variant === "secondary"
-                ? "#F6F6F6"
-                : "#fff"),
+            (variant === 'primary'
+                ? 'red'
+                : variant === 'secondary'
+                ? '#4D4D4D'
+                : '#fff'),
         ...filterProps(attr),
     };
 
